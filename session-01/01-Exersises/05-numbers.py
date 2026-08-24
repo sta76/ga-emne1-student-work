6687b336ceec38fdfdc2bd2a0220aafb84c8da54
Ståle Sætre <sta.sat791@gokstadakademiet.no>
2026-08-24 21:22:42 +0200

    Added first lesson exercises rev-1a

--- session-01/01-Exersises/05-numbers.py
+++ session-01/01-Exersises/05-numbers.py
@@ -1,6 +1,7 @@
-number1 = int(input("Enter a number: "))
-number2 = int(input("Enter a second number: "))
+number1, number2 = map(float, input("Enter two number separated with space: ").split())
 
+#number1 = int(number1)
+#number2 = int(number2)
 addition = number1 + number2
 multiplication = number1 * number2
 

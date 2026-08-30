15ad86473c7dc71b59d0032f13d59fe5b45fa516
sta76 <sta.sat791@gokstadakademiet>
2026-08-30 11:04:55 +0200

    oppgaveløsning

---
+++ session-01/01-Exersises/sentence.py
@@ -0,0 +1,6 @@
+adjektiv1 = input("Skriv inn ett adjektiv: ")
+adjektiv2 = input("Skriv inn ett nytt adjektiv: ")
+substantiv = input("Skriv inn ett substantiv: ")
+verb = input("Skriv inn ett verb: ")
+setning = f"den {adjektiv1} {substantiv} {verb} under det {adjektiv2} bordet."
+print(setning.capitalize())

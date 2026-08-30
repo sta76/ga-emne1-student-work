7dfc9937bd64b53f3e25c406c899114d422a7afe
sta76 <sta.sat791@gokstadakademiet>
2026-08-30 13:05:50 +0200

    oppgaveløsning

--- session-01/01-Exersises/sec_to_min_to_hour.py
+++ session-01/01-Exersises/sec_to_min_to_hour.py
@@ -1,7 +1,7 @@
 seconds = int(input("Enter in seconds: "))
 
 hours = seconds // 3600
-min = (seconds -3600)  % 60
+min = (seconds -3600)  // 60
 sec_left = seconds % 60
 
 print(f"{seconds} is {hours} hour, {min} minutes and {sec_left} seconds")
